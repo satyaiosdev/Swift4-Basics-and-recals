6656c01a153e650804b512d97e3d9c8dcd870ac3
Satya <satya.iosdev@gmail.com>
2018-06-05 17:16:06 -0400

    Initial Commit

---
+++ Swift4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift4
+//
+//  Created by Satya Venkata Krishna Achanta on 6/5/18.
+//  Copyright © 2018 Satya Venkata Krishna Achanta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
